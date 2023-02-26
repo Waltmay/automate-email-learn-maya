d44763fbd767bd80d7a16669588178ffba28b0e1
Waltmay <wmay43@gmail.com>
2023-02-26 00:20:12 -0800

    Reorder functions for sampling a word using `main()`

--- main.py
+++ main.py
@@ -15,45 +15,15 @@ email_sender = os.environ.get('EMAIL_SENDER')
 email_password = os.environ.get('EMAIL_PW')
 
 
-def subset_to_unchosen_words(all_words):
-    if Path('./past-words.csv').is_file():
-        past_words_df = pd.read_csv('past-words.csv', encoding='cp1252')
-        past_words_list = past_words_df['word_id'].tolist()
-        unchosen_words = all_words[~all_words.word_id.isin(past_words_list)]
-        return unchosen_words
-    else:
-        return all_words
-
-
-def determine_word_to_send():
-    # Load in with Windows-1252 encoding given the CSV's special characters
-    all_words = pd.read_csv('yucatecan-maya-definitions.csv', encoding='cp1252').reset_index(drop=True)
-    unchosen_words = subset_to_unchosen_words(all_words)
-    word_definition_pair = unchosen_words.sample(n=1)
-    word = word_definition_pair['words'].values[0].title()
-    definition = word_definition_pair['definitions'].values[0]
-    word_id = word_definition_pair['word_id'].values[0]
-    return word, definition, word_id
-
-
-word, definition, word_id = determine_word_to_send()
-
-
-def record_word_def_chosen(word, definition, word_id):
-    fieldnames = ['word_id', 'word', 'definition', 'date']
-    if Path('./past-words.csv').is_file():
-        with open('past-words.csv', 'a', encoding='cp1252', newline='') as past_words_csv:
-            writer = csv.DictWriter(past_words_csv, fieldnames=fieldnames)
-            writer.writerow({'word_id': word_id, 'word': word, 'definition': definition, 'date': date.today()})
-    else:
-        with open('past-words.csv', 'a', encoding='cp1252', newline='') as past_words_csv:
-            writer = csv.DictWriter(past_words_csv, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow({'word_id': word_id, 'word': word, 'definition': definition, 'date': date.today()})
 
 
 
-record_word_def_chosen(word, definition, word_id)
+
+
+
+
+
+
 
 # Subject line of email
 subject = f"Maya Word of the Day: {date.today()}"
@@ -98,3 +68,50 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     email_list = json.loads(os.environ['EMAIL_LIST'])
     for email_receiver in email_list:
         smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
+def main():
+    word, definition, word_id = determine_word_to_send()
+    record_word_def_chosen(word, definition, word_id)
+
+
+def determine_word_to_send():
+    # Load in with Windows-1252 encoding given the CSV's special characters
+    all_words = pd.read_csv('yucatecan-maya-definitions.csv', encoding='cp1252').reset_index(drop=True)
+
+    # Randomly select one word from all words that haven't been chosen before
+    unchosen_words = subset_to_unchosen_words(all_words)
+    word_definition_pair = unchosen_words.sample(n=1)
+
+    # Grab the word and definition values from the randomly selected row
+    word = word_definition_pair['words'].values[0].title()
+    definition = word_definition_pair['definitions'].values[0]
+    word_id = word_definition_pair['word_id'].values[0]
+    return word, definition, word_id
+
+
+def subset_to_unchosen_words(all_words):
+    if Path('./past-words.csv').is_file():
+        past_words_df = pd.read_csv('past-words.csv', encoding='cp1252')
+        past_words_list = past_words_df['word_id'].tolist()
+        unchosen_words = all_words[~all_words.word_id.isin(past_words_list)]
+        return unchosen_words
+    else:
+        return all_words
+
+
+def record_word_def_chosen(word, definition, word_id):
+    fieldnames = ['word_id', 'word', 'definition', 'date']
+    if Path('./past-words.csv').is_file():
+        with open('past-words.csv', 'a', encoding='cp1252', newline='') as past_words_csv:
+            writer = csv.DictWriter(past_words_csv, fieldnames=fieldnames)
+            writer.writerow({'word_id': word_id, 'word': word, 'definition': definition, 'date': date.today()})
+    else:
+        with open('past-words.csv', 'a', encoding='cp1252', newline='') as past_words_csv:
+            writer = csv.DictWriter(past_words_csv, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'word_id': word_id, 'word': word, 'definition': definition, 'date': date.today()})
+
+
+if __name__ == '__main__':
+    main()
